d0745dd407e32134e84c80c9dc6852f7218d07cb
AIniuseva <anz.iniu@gmail.com>
2021-02-05 19:06:32 +0200

    Added gaming field

--- tic-tac-toe/src/main/java/com/app/TicTacToe.java
+++ tic-tac-toe/src/main/java/com/app/TicTacToe.java
@@ -1,7 +1,32 @@
 package com.app;
 
+import java.util.Scanner;
+
 public class TicTacToe {
     public static void main(String[] args) {
-        System.out.println("o_O");
+        Scanner scanner = new Scanner(System.in);
+
+        String input;
+        input = scanner.nextLine();
+        char[][] ticTacToeFiled = new char[3][3];
+
+        int k = 0;
+
+        for (int i = 0; i < ticTacToeFiled.length; i++) {
+            for (int j = 0; j < ticTacToeFiled[0].length; j++) {
+                ticTacToeFiled[i][j] = input.charAt(k);
+                k++;
+            }
+        }
+
+        System.out.println("---------");
+        for (int i = 0; i < ticTacToeFiled.length; i++) {
+            System.out.print("| ");
+            for (int j = 0; j < ticTacToeFiled[0].length; j++) {
+                System.out.print(ticTacToeFiled[i][j] + " ");
+            }
+            System.out.println("|");
+        }
+        System.out.println("---------");
     }
 }
