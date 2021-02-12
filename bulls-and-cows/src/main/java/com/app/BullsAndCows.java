0995a2afca7de5877623bbeebd6dfa1eeef2b063
AIniuseva <anz.iniu@gmail.com>
2021-02-12 15:40:24 +0200

    Added game mode, unique number generator

--- bulls-and-cows/src/main/java/com/app/BullsAndCows.java
+++ bulls-and-cows/src/main/java/com/app/BullsAndCows.java
@@ -5,40 +5,85 @@ import java.util.Scanner;
 
 public class BullsAndCows {
 
+    private static final Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+
         Random random = new Random();
+        int randomNumberLength;
 
-        int secretCode = random.nextInt(9999 - 1000 + 1) + 1000;
-        String guess = scanner.nextLine();
-        System.out.println(secretCode);
+        do {
+            System.out.println("Please, enter the secret code's length:");
+            randomNumberLength = scanner.nextInt();
+            if (randomNumberLength > 10) {
+                System.out.printf("Error: can't generate a secret number with a length of " +
+                        "%d because there aren't enough unique digits.%n", randomNumberLength);
+            }
+        } while (randomNumberLength > 10);
+        scanner.nextLine();
 
-        System.out.println(grader(guess, String.valueOf(secretCode)));
-    }
+        int randomRange = (int) (Math.pow(10, --randomNumberLength));
 
-    private static String grader(String guess, String secretCode) {
-        int bulls = 0;
-        int cows = 0;
+        String secretCode = null;
+        boolean isContainsTheSameNumber = true;
 
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 4; j++) {
-                boolean equal = secretCode.charAt(i) == guess.charAt(j);
-                if (i == j && equal) {
-                    bulls++;
-                } else if (equal) {
-                    cows++;
+        //unique number generator:
+        while (isContainsTheSameNumber) {
+            secretCode = String.valueOf(random.nextInt(((randomRange * 10) - 1) - randomRange + 1) + randomRange);
+
+            if (secretCode.length() == 1) {
+                break;
+            }
+            for (int i = 0; i < secretCode.length() - 1; i++) {
+                StringBuilder secretCodeSave = new StringBuilder(secretCode);
+                char numberForCheck = secretCode.charAt(i);
+                secretCodeSave.deleteCharAt(i);
+                isContainsTheSameNumber = secretCodeSave.toString().contains(String.valueOf(numberForCheck));
+
+                if (isContainsTheSameNumber) {
+                    break;
                 }
             }
         }
 
-        if (bulls > 0 && cows == 0) {
-            return "Grade: " + bulls + " bulls(s). The secret code is " + secretCode;
-        } else if (cows > 0 && bulls == 0) {
-            return "Grade: " + cows + " cow(s). The secret code is " + secretCode;
-        } else if (bulls > 0 && cows > 0) {
-            return "Grade: " + bulls + " bull(s) and " + cows + " cow(s). The secret code is " + secretCode;
-        } else {
-            return "None. The secret code is " + secretCode;
+        String playerGuess = null;
+        int turn = 1;
+
+        System.out.println("Okay, let's start a game!");
+
+        while (!secretCode.equals(playerGuess)) {
+            System.out.printf("Turn %d:%n", turn);
+            playerGuess = scanner.nextLine();
+            resultChecker(playerGuess, secretCode);
+            turn++;
         }
+
+        System.out.println("Congratulations! You guessed the secret code.");
+    }
+
+    private static String resultChecker(String guess, String secretCode) {
+        int bulls = 0;
+        int cows = 0;
+
+        for (int i = 0; i < secretCode.length(); i++) {
+            if (secretCode.charAt(i) == guess.charAt(i)) {
+                bulls++;
+            } else if (secretCode.contains(String.valueOf(guess.charAt(i)))) {
+                cows++;
+            }
+        }
+
+        if (bulls > 0 && cows == 0) {
+            System.out.println("Grade: " + bulls + " bulls(s).");
+            return secretCode;
+        } else if (cows > 0 && bulls == 0) {
+            System.out.println("Grade: " + cows + " cow(s).");
+        } else if (cows > 0 && bulls > 0) {
+            System.out.println("Grade: " + bulls + " bull(s) and " + cows + " cow(s)");
+        } else {
+            System.out.println("None.");
+        }
+        return guess;
     }
 }
+
