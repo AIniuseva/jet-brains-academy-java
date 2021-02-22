9f8a3aa4661ab30fe3a901066098cf8cd2e9a8bc
AIniuseva <anz.iniu@gmail.com>
2021-02-22 17:40:31 +0200

    Added simple text complexity assessment

--- readability-score/src/main/java/com/app/ReadabilityScore.java
+++ readability-score/src/main/java/com/app/ReadabilityScore.java
@@ -1,7 +1,27 @@
 package com.app;
 
+import java.util.Scanner;
+
 public class ReadabilityScore {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String textForEstimation = scanner.nextLine();
 
+        String[] words = textForEstimation.split("\\s+");
+        int totalWords = words.length;
+        String sentences = textForEstimation.replaceAll("[^.!?]", "");
+        int totalSentences = sentences.length();
+
+        String lastWord = String.valueOf(textForEstimation.charAt(textForEstimation.length() - 1)).replaceAll("\\w+", "");
+        if(!lastWord.matches("[.!?]")){
+            totalSentences++;
+        }
+
+        double averageWordsInSentence = (double)totalWords / (double)totalSentences;
+        if(averageWordsInSentence > 10){
+            System.out.println("HARD");
+        }else{
+            System.out.println("EASY");
+        }
     }
 }
