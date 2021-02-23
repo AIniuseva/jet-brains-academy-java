ff434c5e54a3a2b291df876838138a99afa7a166
AIniuseva <anz.iniu@gmail.com>
2021-02-23 20:51:45 +0200

    Added readability test

--- readability-score/src/main/java/com/app/ReadabilityScore.java
+++ readability-score/src/main/java/com/app/ReadabilityScore.java
@@ -1,52 +1,44 @@
 package com.app;
 
+import com.app.formulas.ReadabilityTest;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.math.RoundingMode;
 import java.text.DecimalFormat;
-import java.util.HashMap;
 import java.util.Scanner;
 
 public class ReadabilityScore {
 
-    private static HashMap<Integer, String> gradeLevel = fillReadabilityIndexAgeMap();
-
     public static void main(String[] args) {
-        File textFile = new File("D:\\Java-projects\\Readability Score\\Readability Score\\task\\src\\readability\\in.txt"); //TODO: CHANGE PATH
+        File textFile = new File(args[0]);
+
         int totalWords = 0;
         int totalSentences = 0;
         int totalCharacters = 0;
-
-        String sentenceCheck;
-        String characterCheck;
         String textLine = "";
 
-        System.out.println("The text is:");
         try (Scanner scanner = new Scanner(textFile)) {
             while (scanner.hasNext()) {
                 textLine = scanner.nextLine();
-
                 System.out.println(textLine);
-
                 String[] words = textLine.split("\\s+");
                 totalWords += words.length;
 
-                sentenceCheck = textLine.replaceAll("[^.!?]", "");
-                totalSentences += sentenceCheck.length();
-
-                characterCheck = textLine.replaceAll("\\s+", "");
-                totalCharacters += characterCheck.length();
+                String checker;
+                checker = textLine.replaceAll("[^.!?]", "");
+                totalSentences += checker.length();
+                checker = textLine.replaceAll("\\s+", "");
+                totalCharacters += checker.length();
             }
         } catch (FileNotFoundException e) {
             System.out.println("File not found: " + textFile);
         }
 
-        String lastWord = String.valueOf(textLine.charAt(textLine.length() - 1)).replaceAll("\\w+", "");
-        if (!lastWord.matches("[.!?]")) {
+        if (String.valueOf(textLine.charAt(textLine.length() - 1)).matches("[^.!?]")) {
             totalSentences++;
         }
 
-        double score = automatedReadabilityIndex(totalWords, totalSentences, totalCharacters);
+        double score = ReadabilityTest.automatedReadabilityIndex(totalWords, totalSentences, totalCharacters);
 
         DecimalFormat decimalFormat = new DecimalFormat("#.##");
         decimalFormat.setRoundingMode(RoundingMode.DOWN);
@@ -56,37 +48,6 @@ public class ReadabilityScore {
         System.out.println("Sentences: " + totalSentences);
         System.out.println("Characters: " + totalCharacters);
         System.out.println("The score is: " + decimalFormat.format(score));
-        System.out.printf("This text should be understood by %s-year-olds.", determineTheAgeOfUnderstanding(score));
 
-    }
-
-    private static HashMap<Integer, String> fillReadabilityIndexAgeMap() {
-        HashMap<Integer, String> gradeLevel = new HashMap<>();
-        File gradeLevelFile = new File("D:\\Java-projects\\Readability Score\\Readability Score\\task\\src\\readability\\gradeLevel.txt");
-
-        try (Scanner scanner = new Scanner(gradeLevelFile)) {
-            while (scanner.hasNext()) {
-                String line = scanner.nextLine();
-
-                String[] values = line.split("\\s+");
-                for (String value : values) {
-                    value.replaceAll("\\s+", "");
-                }
-                gradeLevel.put(Integer.valueOf(values[0]), values[1]);
-            }
-
-        } catch (FileNotFoundException e) {
-            System.out.println("File not fount: " + gradeLevelFile);
-        }
-        return gradeLevel;
-    }
-
-    private static double automatedReadabilityIndex(int totalWords, int totalSentences, int totalCharacters) {
-        return 4.71 * ((double) totalCharacters / totalWords) + 0.5 * ((double) totalWords / totalSentences) - 21.43;
-    }
-
-    private static String determineTheAgeOfUnderstanding(double score) {
-        int roundScore = (int) Math.round(score);
-        return gradeLevel.get(roundScore);
     }
 }
---
+++ readability-score/src/main/java/com/app/formulas/ReadabilityTest.java
@@ -0,0 +1,52 @@
+package com.app.formulas;
+
+import java.util.HashMap;
+
+public class ReadabilityTest {
+    private static final HashMap<Integer, Integer> gradeLevel = new HashMap<>();
+
+    private ReadabilityTest() {
+    }
+
+    public static double automatedReadabilityIndex(int totalWords, int totalSentences, int totalCharacters) {
+        return 4.71 * ((double) totalCharacters / totalWords) + 0.5 * ((double) totalWords / totalSentences) - 21.43;
+    }
+
+    public static double fleschKincaidReadabilityTest(int totalWords, int totalSentences, int totalSyllables) {
+        return 0.39 * ((double) totalWords / totalSentences) + 11.8 * ((double) totalSyllables / totalWords) - 15.59;
+    }
+
+    public static double SimpleMeasureOfGobbledygook(int totalSentences, int totalPolysyllables) {
+        return 1.043 * Math.sqrt((double) totalPolysyllables * (double) 30 / totalSentences) + 3.1291;
+    }
+
+    public static double colemanLiauIndex(double averageCharactersPer100words, double averageSentencesFer100words) {
+        return 0.0588 * averageCharactersPer100words - 0.296 * averageSentencesFer100words - 15.8;
+    }
+
+    public static int ageOfUnderstanding(double score) {
+        int roundScore = (int) Math.ceil(score);
+        if(roundScore > 14){
+            return gradeLevel.get(14);
+        }
+        return gradeLevel.get(roundScore);
+    }
+
+    static {
+        gradeLevel.put(1, 5);
+        gradeLevel.put(2, 7);
+        gradeLevel.put(3, 9);
+        gradeLevel.put(4, 10);
+        gradeLevel.put(5, 11);
+        gradeLevel.put(6, 12);
+        gradeLevel.put(7, 13);
+        gradeLevel.put(8, 14);
+        gradeLevel.put(9, 15);
+        gradeLevel.put(10, 16);
+        gradeLevel.put(11, 17);
+        gradeLevel.put(12, 18);
+        gradeLevel.put(13, 24);
+        gradeLevel.put(14, 30);
+    }
+}
+
