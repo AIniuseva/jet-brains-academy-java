d4510bd4e93bd7d71f5e9b2f14d0b4af45ba61f6
AIniuseva <anz.iniu@gmail.com>
2021-03-06 22:09:29 +0200

    Added unicode, shift dec and enc

--- encryption-decryption/src/main/java/com/app/service/Decryption.java
+++ encryption-decryption/src/main/java/com/app/service/Decryption.java
@@ -1,19 +1,31 @@
 package com.app.service;
 
-import com.app.model.EncryptionDecryptionModel;
+import com.app.service.DecEncStrategy;
 
-public class Decryption extends EncryptionDecryptionModel {
-
-    public Decryption(String message) {
-        super(message);
-    }
-
-    public StringBuilder keyDecryptor(int key) {
+public class Decryption implements DecEncStrategy {
+    @Override
+    public StringBuilder messageUnicodeChanging(String message, int key) {
         StringBuilder decryptedMessage = new StringBuilder();
-
-        for (int i = 0; i < this.getMessage().length(); i++) {
-            decryptedMessage.append((char) (this.getMessage().charAt(i) - key));
+        for (int i = 0; i < message.length(); i++) {
+            decryptedMessage.append((char) (message.charAt(i) - key));
         }
         return decryptedMessage;
+    }
+
+    @Override
+    public StringBuilder messageShiftChanging(String message, int key) {
+        StringBuilder encryptedMessage = new StringBuilder();
+        for (int i = 0; i < message.length(); i++) {
+            if (String.valueOf(message.charAt(i)).matches("[!.,?\\s+]")) {
+                encryptedMessage.append(message.charAt(i));
+            } else if (String.valueOf(message.charAt(i)).matches("[a-z]") && message.charAt(i) - key < 'a') {
+                encryptedMessage.append((char) ('z' + 1 - key + (message.charAt(i) - 'a')));
+            } else if (String.valueOf(message.charAt(i)).matches("[A-Z]") && message.charAt(i) - key < 'A') {
+                encryptedMessage.append((char) ('Z' + 1 - key + (message.charAt(i) - 'A')));
+            } else {
+                encryptedMessage.append((char) (message.charAt(i) - key));
+            }
+        }
+        return encryptedMessage;
     }
 }
--- encryption-decryption/src/main/java/com/app/service/Encryption.java
+++ encryption-decryption/src/main/java/com/app/service/Encryption.java
@@ -1,19 +1,34 @@
 package com.app.service;
 
-import com.app.model.EncryptionDecryptionModel;
+import com.app.service.DecEncStrategy;
 
-public class Encryption extends EncryptionDecryptionModel {
-
-    public Encryption(String message) {
-        super(message);
-    }
-
-    public StringBuilder keyEncryptor(int key) {
+public class Encryption implements DecEncStrategy {
+    @Override
+    public StringBuilder messageUnicodeChanging(String message, int key) {
         StringBuilder encryptedMessage = new StringBuilder();
-
-        for (int i = 0; i < this.getMessage().length(); i++) {
-            encryptedMessage.append((char) (this.getMessage().charAt(i) + key));
+        for (int i = 0; i < message.length(); i++) {
+            encryptedMessage.append((char) (message.charAt(i) + key));
         }
         return encryptedMessage;
     }
+
+    @Override
+    public StringBuilder messageShiftChanging(String message, int key) {
+        StringBuilder encryptedMessage = new StringBuilder();
+        for (int i = 0; i < message.length(); i++) {
+            if (String.valueOf(message.charAt(i)).matches("[!.,?\\s+]")) {
+                encryptedMessage.append(message.charAt(i));
+            } else if (String.valueOf(message.charAt(i)).matches("[a-z]") && message.charAt(i) + key > 'z') {
+                encryptedMessage.append((char) ('a' - 1 + key - ('z' - message.charAt(i))));
+            } else if (String.valueOf(message.charAt(i)).matches("[A-Z]") && message.charAt(i) + key > 'Z') {
+                encryptedMessage.append((char) ('A' - 1 + key - ('Z' - message.charAt(i))));
+            } else {
+                encryptedMessage.append((char) (message.charAt(i) + key));
+            }
+        }
+        return encryptedMessage;
+    }
+
 }
+
+
