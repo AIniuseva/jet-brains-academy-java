25dc20e84fe5ecf855ffd367c8fe60866cffb233
AIniuseva <anz.iniu@gmail.com>
2021-03-27 15:31:33 +0200

    Added pin code checker

--- simple-banking-system/src/main/java/com/app/database/Database.java
+++ simple-banking-system/src/main/java/com/app/database/Database.java
@@ -1,14 +1,12 @@
 package com.app.database;
 
+import com.app.Account;
 import org.sqlite.SQLiteDataSource;
 
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class Database {
     private final String databaseUrl;
-    private String tableName;
 
     public Database(String databaseUrl) {
         this.databaseUrl = databaseUrl;
@@ -16,13 +14,12 @@ public class Database {
         SQLiteDataSource dataSource = new SQLiteDataSource();
         dataSource.setUrl(databaseUrl);
         try (Connection con = dataSource.getConnection()) {
-            try (Statement statement = con.createStatement()) {
+            try (Statement statement = con.createStatement()) { //TODO: change to prepare statement
                 statement.executeUpdate("CREATE TABLE IF NOT EXISTS card(" +
-                        "id INTEGER," +
+                        "id INTEGER PRIMARY KEY ," +
                         "number TEXT NOT NULL," +
                         "pin TEXT NOT NULL," +
                         "balance INTEGER DEFAULT 0);");
-                this.tableName = "card";
             } catch (SQLException e) {
                 e.printStackTrace();
             }
@@ -44,7 +41,31 @@ public class Database {
         } catch (SQLException e) {
             e.printStackTrace();
         }
+    }
 
+    public boolean checkPinCode(String number, String pin) {
+        SQLiteDataSource dataSource = new SQLiteDataSource();
+        dataSource.setUrl(databaseUrl);
+
+        String select = "SELECT pin FROM card WHERE number=(?)";
+
+        try (Connection con = dataSource.getConnection()) {
+            try (PreparedStatement preparedStatement = con.prepareStatement(select)) {
+                preparedStatement.setString(1,number);
+                ResultSet table = preparedStatement.executeQuery();
+                String realPin = table.getString("pin");
+                System.out.println(pin);
+
+                return pin.equals(realPin);
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+                return false;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 }
 
--- simple-banking-system/src/main/java/com/app/menu/MenuImpl.java
+++ simple-banking-system/src/main/java/com/app/menu/MenuImpl.java
@@ -7,12 +7,17 @@ import java.util.Scanner;
 
 public class MenuImpl implements MenuInterface {
     private Account account;
-    private Database database;
+    private final Database database = new Database("jdbc:sqlite:D:\\Java-projects\\jet-brains-academy-java\\simple-banking-system\\src\\main\\resources\\bankdatabase.db");
+
+    private Account currentAccount;
+
+    public void setCurrentAccount(Account currentAccount) {
+        this.currentAccount = currentAccount;
+    }
 
     @Override
     public void accountCreation() {
         account = new Account();
-        database = new Database("jdbc:sqlite:D:\\Java-projects\\jet-brains-academy-java\\simple-banking-system\\src\\main\\resources\\bankdatabase.db");
         database.addNewCard(account.getCardNumber(), String.valueOf(account.getPinCode()));
 
         System.out.println("Your card has been created");
@@ -25,27 +30,31 @@ public class MenuImpl implements MenuInterface {
 
     @Override
     public void logIntoAccount() {
-        if (account != null) {
-            System.out.println("Enter your card number:");
+        System.out.println("Enter your card number:");
 
-            String cardNumber = scanner.next();
-            System.out.println("Enter your PIN:");
-            int pinCode = scanner.nextInt();
-            if (cardNumber.equals(account.getCardNumber()) && pinCode == account.getPinCode()) {
-                System.out.println("You have successfully logged in!");
-                accountMenu();
-            } else {
-                System.out.println("Wrong card number or PIN!");
-            }
+        String cardNumber = scanner.next();
+        System.out.println("Enter your PIN:");
+        String pinCode = scanner.next();
+
+        if (database.checkPinCode(cardNumber,pinCode)){
+            System.out.println("You have successfully logged in!");
+            setCurrentAccount(account);
+            accountMenu();
+        } else {
+            System.out.println("Wrong card number or PIN!");
         }
+
     }
 
     @Override
     public void accountMenu() {
         while (true) {
-            System.out.println("1. Balance");
-            System.out.println("2. Log out");
-            System.out.println("0. Exit");
+            System.out.println("1. Balance\n" +
+                    "2. Add income\n" +
+                    "3. Do transfer\n" +
+                    "4. Close account\n" +
+                    "5. Log out\n" +
+                    "0. Exit");
             int menuChoice = scanner.nextInt();
 
             switch (menuChoice) {
@@ -53,6 +62,15 @@ public class MenuImpl implements MenuInterface {
                     System.out.println(account.getBalance());
                     break;
                 case 2:
+
+                    break;
+                case 3:
+
+                    break;
+                case 4:
+
+                    break;
+                case 5:
                     return;
                 case 0:
                     System.exit(0);
